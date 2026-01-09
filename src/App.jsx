b07d03e23dfa2c0deffe03996012e80148f6edc8
zachar-AI <zachskihill@gmail.com>
2026-01-09 17:26:54 -0500

    starting a tic tac toe game

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,8 @@
 import { Board } from "./components/TikTakToePieces.jsx";
 
-import { useState } from 'react';
-
-function MyButton({ count, onClick }) {
-  return(
-    <button onClick={onClick}>
-      Clicked {count} times!
-    </button>
-  ); 
-}
-
-function MyButtons() {
-  
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
-  return (
-    <div>
-      <MyButton count={count} onClick={handleClick} />
-      <MyButton count={count} onClick={handleClick} />
-    </div>
-  )
-}
-
 const App = () => {
   return (
-    <MyButtons />
+    <Board />
   )
 };
 export default App;
