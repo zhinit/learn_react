c588b10eed54ae786c228fd58a1a9cf45f5922a9
zachar-AI <zachskihill@gmail.com>
2026-01-09 15:57:26 -0500

    playing with button and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Board } from "./components/TikTakToePieces.jsx";
 
+import { useState } from 'react';
+
 function MyButton() {
+  const [count, setCount] = useState(0);
   function handleClick() {
-    alert("You clicked me!");
+    setCount(count + 1);
   }
 
   return(
-    <button>
-      Click me
+    <button onClick={handleClick}>
+      Clicked {count} times!
     </button>
   ); 
 }
