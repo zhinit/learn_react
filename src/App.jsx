dc4ee31c89bf73c7a8b2ee08136de4aa8348f9d2
zachar-AI <zachskihill@gmail.com>
2026-01-09 00:52:09 -0500

    cleared app.jsx so can start project

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,4 @@
 const App = () => {
-  const name = "Zach"
-  const x = 10;
-  const y = 20;
-  const names = ['Brad', 'Mary', 'Joe', 'Sara'];
-  const loggedIn = true;
-  const styles = {
-    color: 'red',
-    fontSize: '55px'
-  }
-
-  return (
-    <>
-      <div className="text-5xl">App</div>
-      <p style={styles}>hello {name}</p>
-      <p>The sum of {x} and {y} is {x+y}</p>
-      <ul>
-        {names.map((name, index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
-      { loggedIn ? <h1>Hello Member</h1> : <h1>Hello Guest</h1> }
-      { loggedIn && <h1>You Are a great member!</h1>}
-    </>
-  )
+  return <div>App</div>
 };
 export default App;
